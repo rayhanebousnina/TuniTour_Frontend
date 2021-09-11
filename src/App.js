adeeab2f8e95ed9a56ad202bebf5e67f54fb206f
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2021-09-11 01:57:05 +0100

    Routes fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import Home from "./Layouts/ClientLayouts";
 import City from "./Layouts/ClientLayouts/City";
 import Service from "./Layouts/ClientLayouts/Service";
@@ -12,14 +17,12 @@ function App() {
   return (
     <div>
       <Router>
-        <Route path="/home" component={Home} />
+        <Route exact path="/home" component={Home} />
         <Route path="/city/:id" component={City} />
         <Route path="/service/:id" component={Service} />
+        <Route path="/admin" component={DashboardRoutes} />
         <Route path="/user" component={RegisterPage} />
-
-        <Switch>
-          <UserPrivateRoute path="/admin" component={DashboardRoutes} />
-        </Switch>
+        <Route path="/login" component={LoginPage} />
       </Router>
     </div>
   );
