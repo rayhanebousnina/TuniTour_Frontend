1ad3d999afc98b4445ae7d5798d62fe57081c635
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2021-09-12 18:26:29 +0100

    city page content

--- src/Layouts/ClientLayouts/City.js
+++ src/Layouts/ClientLayouts/City.js
@@ -5,6 +5,13 @@ import { Container, Row, Col, Image } from "react-bootstrap";
 import NavigationBar2 from "../../Components/Navbars/Navbar2";
 import "./City.css";
 import { getCityById } from "../../Redux/Actions/cityActions";
+import Footer from "../../Components/Footer/Footer";
+import MonastirData from "./metadata/Monastir.json";
+import TunisData from "./metadata/Tunis.json";
+import KairouanData from "./metadata/Kairaoun.json";
+import DjerbaData from "./metadata/Djerba.json";
+import SousseData from "./metadata/Sousse.json";
+import HammametData from "./metadata/Hammamet.json";
 
 const City = () => {
   const dispatch = useDispatch();
@@ -16,7 +23,36 @@ const City = () => {
 
   const city = useSelector((state) => state.citiesReducer.city);
   console.log("onecity", city);
-
+  var myData = [
+    {
+      text1: "aaa",
+    },
+  ];
+  if (city.length !== 8) {
+    var myCity = city.cityName;
+    switch (myCity) {
+      case "Tunis":
+        myData = TunisData;
+        break;
+      case "Monastir":
+        myData = MonastirData;
+        break;
+      case "Kairouan":
+        myData = KairouanData;
+        break;
+      case "Djerba":
+        myData = DjerbaData;
+        break;
+      case "Sousse":
+        myData = SousseData;
+        break;
+      case "Hammamet":
+        myData = HammametData;
+        break;
+    }
+  } else {
+    console.log("falseeeeee");
+  }
   return (
     <div>
       {/* Landing city section */}
@@ -33,11 +69,32 @@ const City = () => {
             </Col>
           </Row>
           <Row>
-            <Col className="mt-5" md={{ span: 8, offset: 4 }}>
+            <Col className="py-4" md={{ span: 8, offset: 4 }}>
               <h2>{city.cityName} City</h2>
+            </Col>
+            <Col md={12} sm={12}>
+              <p className="city-text">{city.cityDescription}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2 className="text-center py-4">Gallery</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6} sm={12}>
+              <Image fluid src={myData[0].image1} className="py-1"></Image>
+              <Image fluid src={myData[0].image2} className="py-1"></Image>
+            </Col>
+            <Col md={6} sm={12}>
+              <Image fluid src={myData[0].image3} className="py-1"></Image>
+              <Image fluid src={myData[0].image4} className="py-1"></Image>
             </Col>
           </Row>
         </Container>
+        <div className="border-footer">
+          <Footer />
+        </div>
       </div>
     </div>
   );
