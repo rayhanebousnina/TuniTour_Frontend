580d74d843d6c94735e4950c4be638271c23942a
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2021-11-04 00:30:13 +0100

    Footer design changed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,27 +6,31 @@ import {
   InputGroup,
   FormControl,
   Button,
+  Image,
 } from "react-bootstrap";
 import "./Footer.css";
 import logo from "../../Assets/TuniTour.png";
+import googlepaly from "../../Assets/download-on-the-app-store-apple.png";
+import applestore from "../../Assets/en_badge_web_generic.png";
 import { Link } from "react-router-dom";
+import { FaFacebookSquare, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <>
       <Container className="footer_style pt-5">
-        <Row className="pt-3">
+        <Row className="pt-2">
           <Col
             className="d-flex justify-content-center mb-5"
             md={4}
             sm={12}
             xs={12}
           >
-            <Container className="ms-5">
-              <Row>
+            <Container className="ms-3">
+              <Row className="d-flex justify-content-center mb-2">
                 <img className="logo" src={logo} />
               </Row>
-              <Row>
+              <Row className="d-flex justify-content-center">
                 TuniTour is a tour guide <br /> to discover the beauty of
                 tunisian cities.
               </Row>
@@ -38,18 +42,18 @@ const Footer = () => {
             sm={12}
             xs={12}
           >
-            <Container className="text-center">
-              <Row>
+            <Container className="">
+              <Row className="mb-3">
                 <h4>Useful links</h4>
               </Row>
               <Row>
                 <ul>
-                  <li>
+                  <li className="mb-3">
                     <Link className="useful_link" to="/home">
                       Home
                     </Link>
                   </li>
-                  <li>
+                  <li className="mb-3">
                     <Link className="useful_link" to="/explorecities">
                       Cities
                     </Link>
@@ -62,9 +66,34 @@ const Footer = () => {
                 </ul>
               </Row>
             </Container>
+            <Container className="">
+              <Row className="mb-3">
+                <h4>Find Us</h4>
+              </Row>
+              <Row>
+                <Image src={googlepaly} />
+              </Row>
+              <Row className="mt-2">
+                <Image src={applestore} />
+              </Row>
+              <Row className="mt-3 text-center">
+                <Col className="circle">
+                  <FaFacebookSquare />
+                </Col>
+                <Col>
+                  <FaTwitter />
+                </Col>
+                <Col>
+                  <FaInstagram />
+                </Col>
+              </Row>
+            </Container>
           </Col>
           <Col md={4} sm={12} xs={12}>
-            <h4>Your travel journey starts here</h4>
+            <div className="mb-4">
+              <h4>Your travel journey starts here</h4>
+            </div>
+
             <p>
               Subscribe now for travel tips, personalized itineraries, and
               vacation inspiration straight to your inbox.
@@ -83,22 +112,16 @@ const Footer = () => {
           </Col>
         </Row>
       </Container>
-      <Container fluid className="text-center">
+      {/* <Container fluid className="text-center">
         <Row className="copyright_section">
-          <Col>
-            <i class="far fa-copyright"></i> TuniTour
-          </Col>
-          <Col>
-            <pre>
-              Legal notice · Terms · Privacy · Cookies
-              <i class="fab fa-facebook-f"></i>
-              <i class="fab fa-instagram"></i>
-              <i class="fab fa-twitter"></i>
-              <i class="fab fa-youtube"></i>
-            </pre>
+          <Col className="d-flex justify-content-center">
+            <p>
+              Copyright <i class="far fa-copyright"></i> 2021 Office National du
+              Tourisme Tunisien
+            </p>
           </Col>
         </Row>
-      </Container>
+      </Container> */}
     </>
   );
 };
